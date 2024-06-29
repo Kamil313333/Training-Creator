4d29c29f419d0a9f43a8b7f5ac8c20930be6d50b
Kamil Krzysztoszek <kamil411313@gmail.com>
2024-06-29 15:59:48 +0200

    Trzeci commit

--- app.py
+++ app.py
@@ -3,36 +3,62 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
+# Model Exercise
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     muscle_group = db.Column(db.String(100), nullable=False)
-    equipment_needed = db.Column(db.String(100), nullable=False)
 
+    def __repr__(self):
+        return f'<Exercise {self.name}>'
+
+# Tworzenie przykładowych ćwiczeń w bazie danych
+def create_sample_exercises():
+    exercises = [
+        Exercise(name='Push-ups', muscle_group='Chest'),
+        Exercise(name='Pull-ups', muscle_group='Back'),
+        Exercise(name='Squats', muscle_group='Legs'),
+        Exercise(name='Plank', muscle_group='Core')
+    ]
+    db.session.bulk_save_objects(exercises)
+    db.session.commit()
+
+# Widok listy ćwiczeń z podziałem na grupy mięśniowe
 @app.route('/')
-def index():
-    return render_template('index.html')
+def exercises():
+    exercises = Exercise.query.all()
+    return render_template('exercises.html', exercises=exercises)
 
+# Widok dodawania nowego ćwiczenia
 @app.route('/add_exercise', methods=['GET', 'POST'])
 def add_exercise():
     if request.method == 'POST':
         name = request.form['name']
         muscle_group = request.form['muscle_group']
-        equipment_needed = request.form['equipment_needed']
 
-        exercise = Exercise(name=name, muscle_group=muscle_group, equipment_needed=equipment_needed)
-        db.session.add(exercise)
+        new_exercise = Exercise(name=name, muscle_group=muscle_group)
+        db.session.add(new_exercise)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for('exercises'))
 
     return render_template('add_exercise.html')
 
+# Widok usuwania ćwiczenia
+@app.route('/delete_exercise/<int:id>', methods=['POST'])
+def delete_exercise(id):
+    exercise = Exercise.query.get_or_404(id)
+    db.session.delete(exercise)
+    db.session.commit()
+    return redirect(url_for('exercises'))
+
+# Inicjalizacja bazy danych
+with app.app_context():
+    db.create_all()
+    create_sample_exercises()
+
+# Uruchomienie aplikacji
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
