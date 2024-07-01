7c1c54704e44a951c996ba19cb1bfa65c763bc81
Kamil Krzysztoszek <kamil411313@gmail.com>
2024-07-01 14:33:11 +0200

    Dzialajacy delete i generowanie planu

--- app.py
+++ app.py
@@ -1,64 +1,93 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-# Model Exercise
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     muscle_group = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return f'<Exercise {self.name}>'
-
-# Tworzenie przykładowych ćwiczeń w bazie danych
-def create_sample_exercises():
-    exercises = [
-        Exercise(name='Push-ups', muscle_group='Chest'),
-        Exercise(name='Pull-ups', muscle_group='Back'),
-        Exercise(name='Squats', muscle_group='Legs'),
-        Exercise(name='Plank', muscle_group='Core')
-    ]
-    db.session.bulk_save_objects(exercises)
-    db.session.commit()
-
-# Widok listy ćwiczeń z podziałem na grupy mięśniowe
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/exercises')
 def exercises():
     exercises = Exercise.query.all()
     return render_template('exercises.html', exercises=exercises)
 
-# Widok dodawania nowego ćwiczenia
-@app.route('/add_exercise', methods=['GET', 'POST'])
+@app.route('/add-exercise', methods=['GET', 'POST'])
 def add_exercise():
     if request.method == 'POST':
         name = request.form['name']
         muscle_group = request.form['muscle_group']
-
         new_exercise = Exercise(name=name, muscle_group=muscle_group)
         db.session.add(new_exercise)
         db.session.commit()
-
         return redirect(url_for('exercises'))
-
     return render_template('add_exercise.html')
 
-# Widok usuwania ćwiczenia
-@app.route('/delete_exercise/<int:id>', methods=['POST'])
+@app.route('/delete-exercise/<int:id>', methods=['GET', 'POST'])
 def delete_exercise(id):
     exercise = Exercise.query.get_or_404(id)
-    db.session.delete(exercise)
-    db.session.commit()
-    return redirect(url_for('exercises'))
+    if request.method == 'POST':
+        db.session.delete(exercise)
+        db.session.commit()
+        return redirect(url_for('exercises'))
+    return render_template('delete_exercise.html', exercise=exercise)
 
-# Inicjalizacja bazy danych
-with app.app_context():
-    db.create_all()
-    create_sample_exercises()
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        training_type = request.form['training_type']
+        return redirect(url_for('plan', training_type=training_type))
+    return render_template('home.html')
 
-# Uruchomienie aplikacji
+@app.route('/plan/<training_type>')
+def plan(training_type):
+    exercises = Exercise.query.all()
+    grouped_exercises = {
+        'Legs': [e for e in exercises if e.muscle_group == 'Legs'],
+        'Shoulders': [e for e in exercises if e.muscle_group == 'Shoulders'],
+        'Chest': [e for e in exercises if e.muscle_group == 'Chest'],
+        'Back': [e for e in exercises if e.muscle_group == 'Back'],
+        'Biceps': [e for e in exercises if e.muscle_group == 'Biceps'],
+        'Triceps': [e for e in exercises if e.muscle_group == 'Triceps'],
+        'Calves': [e for e in exercises if e.muscle_group == 'Calves']
+    }
+    plan = generate_plan(training_type, grouped_exercises)
+    return render_template('plan.html', training_type=training_type, plan=plan)
+
+def generate_plan(training_type, exercises):
+    plan = {}
+    if training_type == 'Push Pull Legs':
+        plan['Push'] = random.sample(exercises['Chest'], min(3, len(exercises['Chest']))) + \
+                       random.sample(exercises['Shoulders'], min(2, len(exercises['Shoulders']))) + \
+                       random.sample(exercises['Triceps'], min(1, len(exercises['Triceps'])))
+        plan['Pull'] = random.sample(exercises['Back'], min(4, len(exercises['Back']))) + \
+                       random.sample(exercises['Biceps'], min(1, len(exercises['Biceps']))) + \
+                       random.sample(exercises['Shoulders'], min(1, len(exercises['Shoulders'])))
+        plan['Legs'] = random.sample(exercises['Legs'], min(5, len(exercises['Legs']))) + \
+                       random.sample(exercises['Calves'], min(1, len(exercises['Calves'])))
+    elif training_type == 'Split':
+        plan['Legs'] = random.sample(exercises['Legs'], min(4, len(exercises['Legs']))) + \
+                       random.sample(exercises['Calves'], min(1, len(exercises['Calves'])))
+        plan['Chest'] = random.sample(exercises['Chest'], min(5, len(exercises['Chest'])))
+        plan['Back'] = random.sample(exercises['Back'], min(5, len(exercises['Back'])))
+        plan['Arms'] = random.sample(exercises['Biceps'], min(2, len(exercises['Biceps']))) + \
+                       random.sample(exercises['Triceps'], min(2, len(exercises['Triceps'])))
+    elif training_type == 'FBW':
+        plan['Day A'] = random.sample(exercises['Legs'], min(1, len(exercises['Legs']))) + \
+                        random.sample(exercises['Chest'], min(1, len(exercises['Chest']))) + \
+                        random.sample(exercises['Back'], min(1, len(exercises['Back'])))
+        plan['Day B'] = random.sample(exercises['Legs'], min(1, len(exercises['Legs']))) + \
+                        random.sample(exercises['Chest'], min(1, len(exercises['Chest']))) + \
+                        random.sample(exercises['Back'], min(1, len(exercises['Back'])))
+    return plan
+
 if __name__ == '__main__':
     app.run(debug=True)
